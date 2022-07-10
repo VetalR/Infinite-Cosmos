0dc73471183890e8fbac3a4338d0502da82b00c0
Vitaliy <spector30000@gmail.com>
2022-07-10 13:20:22 +0300

    Add function 'get_space_x_start_image'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 
 def download_image(url, path):
@@ -12,8 +13,21 @@ def download_image(url, path):
         f.write(response.content)
 
 
+def get_space_x_start_image():
+
+    url = 'https://api.spacexdata.com/v3/launches'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    for start in response.json():
+        if start["links"]["flickr_images"]:
+            pprint(start["links"]["flickr_images"], sort_dicts=False)
+
+
 if __name__ == '__main__':
 
-    urls = 'https://dvmn.org/media/HST-SM4.jpeg'
-    img_path = 'images/'
-    download_image(urls, img_path)
+    get_space_x_start_image()
+
+    # urls = 'https://dvmn.org/media/HST-SM4.jpeg'
+    # img_path = 'images/'
+    # download_image(urls, img_path)
