b87682a6613a4212ef4b5543c9c431698f1029c3
Vitaliy <spector30000@gmail.com>
2022-07-15 21:57:13 +0300

    Add function 'get_nasa_astronomy_epic_picture'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
 import os
 from datetime import datetime
+from pprint import pprint
 import requests
 from dotenv import load_dotenv
 import fnmatch
@@ -76,7 +77,7 @@ def get_file_format(url):
 def get_nasa_astronomy_picture_of_the_day(api_key):
 
     url = 'https://api.nasa.gov/planetary/apod'
-    # url = 'https://api.nasa.gov/EPIC/api/natural/images'
+
     params = {
         'api_key': api_key,
         'hd': True,
@@ -102,14 +103,54 @@ def get_nasa_astronomy_picture_of_the_day(api_key):
             files_count += 1
 
 
+def get_nasa_astronomy_epic_picture(api_key):
+
+    epic_url = 'https://api.nasa.gov/EPIC/api/natural/images'
+
+    params = {
+        'api_key': api_key
+    }
+    response = requests.get(
+        url=epic_url,
+        params=params
+    )
+
+    epic_list = response.json()[:10]
+    dir_image = os.path.abspath('images')
+    files_count = len(fnmatch.filter(os.listdir(dir_image), 'nasa_epic_*'))
+    params = {
+        'api_key': api_key
+    }
+
+    for image in epic_list:
+
+        img_name = image['image']
+        img_date = image['date'].split()[0].replace('-', '/')
+
+        img_url = (
+            f'https://api.nasa.gov/EPIC/archive/natural/'
+            f'{img_date}/png/{img_name}.png'
+        )
+        response = requests.get(
+            url=img_url,
+            params=params
+        )
+        response.raise_for_status()
+
+        file_name = f'nasa_epic_{files_count}.png'
+        with open(f'{dir_image}/{file_name}', 'wb') as f:
+            f.write(response.content)
+        files_count += 1
+
 
 if __name__ == '__main__':
 
     load_dotenv()
     api_key_nasa = os.getenv('API_KEY_NASA')
 
-    # now = datetime.now()
+    now = datetime.now()
     # get_nasa_astronomy_picture_of_the_day(api_key_nasa)
-    # print(datetime.now()-now)
+    get_nasa_astronomy_epic_picture(api_key_nasa)
+    print(datetime.now()-now)
 
-    fetch_spacex_last_launch()
+    # fetch_spacex_last_launch()
